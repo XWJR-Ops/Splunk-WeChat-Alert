acb80f75a66081c69628480445d38a3f2f7945ff
Mars Liu <mars@MacBook>
2017-11-30 16:52:17 +0800

    first commit

--- splunk.py
+++ splunk.py
@@ -3,6 +3,7 @@
 #Auther:xwjr.com
 import sys,gzip,csv,json
 import requests
+import time
 import urllib3
 urllib3.disable_warnings()
 
@@ -13,7 +14,7 @@ def get_token(corp_id, secret):
     return token
 
 
-def send_message(splunk_log_info, tag_id, agent_id, token):
+def send_message(message, tag_id, agent_id, token):
     url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % token
     Header = "splunk提醒！！"
     data = {
@@ -21,7 +22,7 @@ def send_message(splunk_log_info, tag_id, agent_id, token):
         "msgtype": "text",
         "agentid": agent_id,
         "text": {
-            "content": Header + '\n' + splunk_log_info
+            "content": Header + '\n' + message
         },
         "safe":"0"
     }
@@ -30,16 +31,18 @@ def send_message(splunk_log_info, tag_id, agent_id, token):
 
 if __name__ == '__main__':
 
+    time_now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     gzip_file = sys.argv[8]
     csv_file = gzip.open(gzip_file)
     csv_read = csv.reader(csv_file)
     for row in csv_read:
-        splunk_log_info = row[2]
+        splunk_log_info = row[3]
+    message = "日志：" + splunk_log_info + '\n时间：' + time_now
 
-    corp_id = "fffffffff"
-    secret = "ddddddd"
-    tag_id = "16"
-    agent_id = "26"
+    corp_id = "changeme"
+    secret = "changeme"
+    tag_id = "changeme"
+    agent_id = "changeme"
 
     token = get_token(corp_id, secret)
-    send_message(splunk_log_info, tag_id, agent_id, token)
+    send_message(message, tag_id, agent_id, token)
